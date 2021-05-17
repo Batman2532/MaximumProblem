ef3cf9d2bc1ad8bd27f35b8afc7e60a132edeb27
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-17 15:11:39 +0530

    Find maximum problem

---
+++ src/main/java/com/maximum/Maximum.java
@@ -0,0 +1,7 @@
+package com.maximum;
+
+public class Maximum {
+    public static void main(String[] args) {
+        System.out.println("Find Maximum problem");
+    }
+}
---
+++ src/test/java/com/maximum/MaximumTest.java
@@ -0,0 +1,8 @@
+package com.maximum;
+
+import org.junit.jupiter.api.Test;
+
+public class MaximumTest {
+    Maximum max = new Maximum();
+
+}
