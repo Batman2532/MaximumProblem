acd1f627988c40430ee42a7450830bfedd953ef0
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-17 16:13:06 +0530

    Refactored: all methods to one generic method

--- src/main/java/com/maximum/Maximum.java
+++ src/main/java/com/maximum/Maximum.java
@@ -5,30 +5,8 @@ public class Maximum {
         System.out.println("Find Maximum problem");
     }
 
-    public Integer maxInteger(Integer i, Integer j, Integer k) {
-        Integer max = i;
-        if(j.compareTo(max)>0){
-            max=j;
-        }
-        if(k.compareTo(max)>0){
-            max=k;
-        }
-        return max;
-    }
-
-    public Float maxFloat(Float i, Float j, Float k) {
-        Float max = i;
-        if(j.compareTo(max)>0){
-            max=j;
-        }
-        if(k.compareTo(max)>0){
-            max=k;
-        }
-        return max;
-    }
-
-    public String maxString(String i, String j, String k){
-        String max = i;
+    public <S extends Comparable> S checkMax(S i, S j, S k) {
+        S max = i;
         if(j.compareTo(max)>0){
             max=j;
         }
--- src/test/java/com/maximum/MaximumTest.java
+++ src/test/java/com/maximum/MaximumTest.java
@@ -9,57 +9,57 @@ public class MaximumTest {
     //Test cases for Integer type
     @Test
     public void firstNumberIsMaxInteger(){
-      Integer maxNo =  max.maxInteger(9,5,3);
+      Integer maxNo =  max.checkMax(9,5,3);
         Assertions.assertEquals(9,maxNo);
     }
 
     @Test
     public void secondNumberIsMaxInteger(){
-        Integer maxNo =  max.maxInteger(9,12,3);
+        Integer maxNo =  max.checkMax(9,12,3);
         Assertions.assertEquals(12,maxNo);
     }
 
     @Test
     public void thirdNumberIsMaxInteger(){
-        Integer maxNo =  max.maxInteger(9,5,11);
+        Integer maxNo =  max.checkMax(9,5,11);
         Assertions.assertEquals(11,maxNo);
     }
 
     //Test cases for Float type
     @Test
     public void firstNumberIsMaxFloat(){
-        Float maxNo =  max.maxFloat(2873.5f,1873.8f,964.7f);
+        Float maxNo =  max.checkMax(2873.5f,1873.8f,964.7f);
         Assertions.assertEquals(2873.5f,maxNo);
     }
 
     @Test
     public void secondNumberIsMaxFloat(){
-        Float maxNo =  max.maxFloat(946.5f,2589.5f,1598.5f);
+        Float maxNo =  max.checkMax(946.5f,2589.5f,1598.5f);
         Assertions.assertEquals(2589.5f,maxNo);
     }
 
     @Test
     public void thirdNumberIsMaxFloat(){
-        Float maxNo =  max.maxFloat(1268.4f,951.6f,2587.3f);
+        Float maxNo =  max.checkMax(1268.4f,951.6f,2587.3f);
         Assertions.assertEquals(2587.3f,maxNo);
     }
 
     //Test cases for String
     @Test
     public void firstStringIsMax(){
-        String maxString =  max.maxString("Zebra","Banana","Mango");
+        String maxString =  max.checkMax("Zebra","Banana","Mango");
         Assertions.assertEquals("Zebra",maxString);
     }
 
     @Test
     public void secondStringIsMax(){
-        String maxString =  max.maxString("Banana","Zebra","Mango");
+        String maxString =  max.checkMax("Banana","Zebra","Mango");
         Assertions.assertEquals("Zebra",maxString);
     }
 
     @Test
     public void thirdStringIsMax(){
-        String maxString =  max.maxString("Banana","Mango","Zebra");
+        String maxString =  max.checkMax("Banana","Mango","Zebra");
         Assertions.assertEquals("Zebra",maxString);
     }
 }
