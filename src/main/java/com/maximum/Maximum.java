c808935b24d627d1d733e58f9f555efe664beae1
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-17 15:30:57 +0530

    Added: Find mazimum from 3 Integers

--- src/main/java/com/maximum/Maximum.java
+++ src/main/java/com/maximum/Maximum.java
@@ -4,4 +4,15 @@ public class Maximum {
     public static void main(String[] args) {
         System.out.println("Find Maximum problem");
     }
+
+    public Integer findMax(Integer i, Integer j, Integer k) {
+        Integer max = i;
+        if(j.compareTo(max)>0){
+            max=j;
+        }
+        if(k.compareTo(max)>0){
+            max=k;
+        }
+        return max;
+    }
 }
--- src/test/java/com/maximum/MaximumTest.java
+++ src/test/java/com/maximum/MaximumTest.java
@@ -1,8 +1,26 @@
 package com.maximum;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MaximumTest {
     Maximum max = new Maximum();
 
+    @Test
+    public void firstNumberIsMax(){
+      Integer maxNo =  max.findMax(9,5,3);
+        Assertions.assertEquals(9,maxNo);
+    }
+
+    @Test
+    public void secondNumberIsMax(){
+        Integer maxNo =  max.findMax(9,12,3);
+        Assertions.assertEquals(12,maxNo);
+    }
+
+    @Test
+    public void thirdNumberIsMax(){
+        Integer maxNo =  max.findMax(9,5,11);
+        Assertions.assertEquals(11,maxNo);
+    }
 }
