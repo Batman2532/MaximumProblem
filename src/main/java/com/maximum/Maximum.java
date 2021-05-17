7c85e0bacc81e36696da6a4ea321b74fea349290
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-17 17:47:20 +0530

    Refactored: to Generic class

--- src/main/java/com/maximum/Maximum.java
+++ src/main/java/com/maximum/Maximum.java
@@ -1,11 +1,23 @@
 package com.maximum;
 
-public class Maximum {
+public class Maximum <S extends Comparable<S>>{
+    S i,j,k;
+    public Maximum(S i,S j, S k){
+        this.i=i;
+        this.j=j;
+        this.k=k;
+    }
+    public S testMaximum(){
+        return Maximum.testMaximum(i,j,k);
+    }
     public static void main(String[] args) {
         System.out.println("Find Maximum problem");
+        new Maximum(95,96,100).testMaximum();
+        new Maximum(252.5f,2563.5f,456.2f).testMaximum();
+        new Maximum("Batman","IronMan","Thor").testMaximum();
     }
 
-    public <S extends Comparable> S checkMax(S i, S j, S k) {
+    public static <S extends Comparable<S>> S testMaximum(S i, S j, S k){
         S max = i;
         if(j.compareTo(max)>0){
             max=j;
@@ -13,6 +25,8 @@ public class Maximum {
         if(k.compareTo(max)>0){
             max=k;
         }
+        System.out.println(max);
         return max;
     }
+
 }
--- src/test/java/com/maximum/MaximumTest.java
+++ src/test/java/com/maximum/MaximumTest.java
@@ -4,62 +4,61 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MaximumTest {
-    Maximum max = new Maximum();
 
     //Test cases for Integer type
     @Test
-    public void firstNumberIsMaxInteger(){
-      Integer maxNo =  max.checkMax(9,5,3);
-        Assertions.assertEquals(9,maxNo);
+    public void firstNumberIsMaxInteger() {
+        Integer maxNo = (Integer) new Maximum(12, 9, 5).testMaximum();
+        Assertions.assertEquals(12, maxNo);
     }
 
     @Test
     public void secondNumberIsMaxInteger(){
-        Integer maxNo =  max.checkMax(9,12,3);
+        Integer maxNo = (Integer) new Maximum(9,12,3).testMaximum();
         Assertions.assertEquals(12,maxNo);
     }
 
     @Test
     public void thirdNumberIsMaxInteger(){
-        Integer maxNo =  max.checkMax(9,5,11);
+        Integer maxNo = (Integer) new Maximum(9,5,11).testMaximum();
         Assertions.assertEquals(11,maxNo);
     }
 
     //Test cases for Float type
     @Test
     public void firstNumberIsMaxFloat(){
-        Float maxNo =  max.checkMax(2873.5f,1873.8f,964.7f);
+        Float maxNo = (Float) new Maximum(2873.5f,1873.8f,964.7f).testMaximum();
         Assertions.assertEquals(2873.5f,maxNo);
     }
 
     @Test
     public void secondNumberIsMaxFloat(){
-        Float maxNo =  max.checkMax(946.5f,2589.5f,1598.5f);
+        Float maxNo = (Float) new Maximum(946.5f,2589.5f,1598.5f).testMaximum();
         Assertions.assertEquals(2589.5f,maxNo);
     }
 
     @Test
     public void thirdNumberIsMaxFloat(){
-        Float maxNo =  max.checkMax(1268.4f,951.6f,2587.3f);
+        Float maxNo = (Float) new Maximum(1268.4f,951.6f,2587.3f).testMaximum();
         Assertions.assertEquals(2587.3f,maxNo);
     }
 
     //Test cases for String
     @Test
     public void firstStringIsMax(){
-        String maxString =  max.checkMax("Zebra","Banana","Mango");
+        String maxString = (String) new Maximum("Zebra","Banana","Mango").testMaximum();
         Assertions.assertEquals("Zebra",maxString);
     }
 
     @Test
     public void secondStringIsMax(){
-        String maxString =  max.checkMax("Banana","Zebra","Mango");
+        String maxString = (String) new Maximum("Banana","Zebra","Mango").testMaximum();
         Assertions.assertEquals("Zebra",maxString);
     }
 
     @Test
     public void thirdStringIsMax(){
-        String maxString =  max.checkMax("Banana","Mango","Zebra");
+        String maxString = (String) new Maximum("Banana","Mango","Zebra").testMaximum();
         Assertions.assertEquals("Zebra",maxString);
     }
 }
